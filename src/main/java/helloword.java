7476530319a6b764ba4cf4d635d4c9b781a90364
Boy <Chen@idea.com>
2022-04-19 23:06:49 +0800

    hotfix commit1

--- src/main/java/helloword.java
+++ src/main/java/helloword.java
@@ -1,5 +1,6 @@
 public class helloword {
     public static void main(String[] args) {
         System.out.println("hello word!1 master commit1");
+        System.out.println("hello word!1 hotfix commit1");
     }
 }
